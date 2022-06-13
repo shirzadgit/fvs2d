aa78cb00c67989be3881109f2ff9d9137dda6dbf
Shirzad Hosseinverdi <103480429+shirzadgit@users.noreply.github.com>
2022-06-13 13:07:17 -0700

    Add files via upload

--- src/mainparam.f90
+++ src/mainparam.f90
@@ -9,14 +9,14 @@ module mainparam
   logical,save            :: lOMP
 
   !-- input file
-  character(len=127)      :: file_input='input.fvm2d'
+  character(len=127)      :: file_input='input.fvs2d'
   integer,parameter       :: iunit_input=900
 
   !-- grid file
   integer,parameter       :: iunit_grid=901
 
   !-- log files
-  character(len=127)      :: file_output='log.fvm2d'
+  character(len=127)      :: file_output='log.fvs2d'
   integer,parameter       :: iunit_output=902
 
 end module mainparam
