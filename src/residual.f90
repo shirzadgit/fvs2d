53031085000cf333231420746b11466776430292
shirzadgit <shirzadh@email.arizona.edu>
2022-07-07 14:21:30 -0700

    module to compute RHS of Euler2D created

--- src/residual.f90
+++ src/residual.f90
@@ -1,204 +1,169 @@
 module residual
 
-  use mainparam,  only  : nvar
+  use mainparam,      only  : nvar
   use input
   use data_grid
-  use data_sol,   only  : pvar
+  use data_solution,  only  : pvar, cvar, resid, grad, cvar2pvar, pvar2cvar
   use flux_invscid
   use gradient
+  use gradient_limiter
+  use mms,            only  : mms_compute_euler2d, vortex_inf
 
   implicit none
-
-  type face_data
-    real,dimension(:),pointer :: f
-  end type
-
-  type(face_data),dimension(:,:),pointer    :: recnst
-
-  private   :: recnst
 
 contains
 
   !============================================================================!
   !\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\!
   !============================================================================!
-  subroutine residual_init
-    implicit none
-
-    integer   :: ivar,ic
-
-    !--------------------------------------------------------------------------!
-    ! allocate array for face value reconstruction
-    !--------------------------------------------------------------------------!
-    allocate( recnst(nvar, ncells) )
-    do ivar=1,nvar
-      do ic=1,ncells
-        allocate( recnst(ivar,ic)%f(cell(ic)%nvrt) )
-      enddo
-    enddo
-
-
-    return
-  end subroutine residual_init
-
-
-  !============================================================================!
-  !\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\!
-  !============================================================================!
-  subroutine compute_residual (resid)
+  subroutine compute_residual
 
     implicit none
-    real,intent(out)  :: resid(nvar,ncells)
-    integer           :: ic,ie,ivar,je, ieL,ieR, icL,icR, k, idum(4), iloc(1)
-    real              :: nxf,nyf,af, flux(nvar), pfL(nvar),pfR(nvar)
-    real,allocatable  :: grad(:,:,:)
+    integer           :: i,ic,ie,ivar,je, ieL,ieR, icL,icR, k, idum(4), iloc(1), ib
+    real              :: flux(nvar), pfL(nvar),pfR(nvar), ws_max
+    real              :: xcL,ycL, xcR,ycR, xf,yf, nxf,nyf, af
+    real,allocatable  :: ws_nrml(:)
 
     resid(:,:) = 0.d0
+
+    allocate( ws_nrml(ncells) )
+
+    !--------------------------------------------------------------------------!
+    ! convert conservatve variables to primative variables
+    !--------------------------------------------------------------------------!
+    call cvar2pvar
 
 
     !--------------------------------------------------------------------------!
     ! compute gradient of primative variables at cell centers
     !--------------------------------------------------------------------------!
-    allocate(grad(nvar,ncells,2))
-    do ivar=1,nvar
-      call gradient_cellcntr(pvar(ivar,1:ncells), grad(ivar,1:ncells,1:2));
-    enddo
+    call compute_gradient_cellcntr
 
 
     !--------------------------------------------------------------------------!
-    ! re-construct face-value of primative variables
+    ! compute gradient limiter
     !--------------------------------------------------------------------------!
-    !-- linear extrapolation
-    if (lface_reconst_linear) then
-      do ivar=1,nvar
-        do ic=1,ncells
-          do ie=1,cell(ic)%nvrt
-            recnst(ivar,ic)%f(ie) = pvar(ivar,ic) + cell(ic)%pos2edg(ie,1) * grad(ivar,ic,1) + cell(ic)%pos2edg(ie,2) * grad(ivar,ic,2)
-          enddo
-        enddo
-      enddo
-
-      !-- apply limiter
-      if (lgrad_limiter) then
-        write(*,*) 'scheme not implemented yet!'
-        stop 'scheme not implemented yet!'
-      endif
-
-    !-- unstructred MUSCL
-    elseif (lface_reconst_umuscl) then
-      write(*,*) 'scheme not implemented yet!'
-      stop 'scheme not implemented yet!'
-    endif
+    call compute_gradient_limiter
 
 
     !--------------------------------------------------------------------------!
-    ! compute left and right fluxes based on re-constructed face-value
+    ! compute left and right inviscid fluxes on interior edges
     !--------------------------------------------------------------------------!
-    interior_cells: do ic=1,ncells_intr
-      icL=cell_intr(ic)
-      do ieL=1,cell(icL)%nvrt
+    interior_edges: do i=1,nedges_intr
+      ie = edge_intr(i)
 
-        je=cell(icL)%edge(ieL)
+      !-- edge properties
+       xf = edge(ie)%x
+       yf = edge(ie)%y
+       af = edge(ie)%area
+      nxf = edge(ie)%nx
+      nyf = edge(ie)%ny
 
-        af =edge(je)%area
-        nxf=edge(je)%nx * cell(icL)%nrmlsign(ieL)
-        nyf=edge(je)%ny * cell(icL)%nrmlsign(ieL)
+      !-- left cell index and coordinates
+      icL = edge(ie)%c1
+      xcL = cell(icL)%x
+      ycL = cell(icL)%y
 
-        !-- find cell and edge number of right state
-        icR=cell(icL)%nghbre(ieL)
-        idum(:)= 1
-        do k=1,cell(icR)%nvrt
-          idum(k)=abs( je - cell(icR)%edge(k) )
-        enddo
-        iloc = minloc(idum)
-        ieR=maxval(iloc)
+      !-- right cell index and coordinates
+      icR = edge(ie)%c2
+      xcR = cell(icR)%x
+      ycR = cell(icR)%y
 
-        !-- construct left and right state on each edge
-        pfL(1:nvar) = recnst(1:nvar,icL)%f(ieL)
-        pfR(1:nvar) = recnst(1:nvar,icR)%f(ieR)
+      !-- re-construct face-value of primative variables
+      pfL(1:nvar) = pvar(1:nvar,icL) + phi_lim(icL) * ((xf-xcL) * grad(1:nvar,icL,1) + (yf-ycL) * grad(1:nvar,icL,2))
+      pfR(1:nvar) = pvar(1:nvar,icR) + phi_lim(icR) * ((xf-xcR) * grad(1:nvar,icR,1) + (yf-ycR) * grad(1:nvar,icR,2))
+
+      !-- compute inviscid flux
+      call compute_flux_invscid (pfL, pfR, nxf,nyf,  flux, ws_max)
+
+      resid(1:nvar,icL) = resid(1:nvar,icL) + flux(1:nvar)*af
+      resid(1:nvar,icR) = resid(1:nvar,icR) - flux(1:nvar)*af
+
+      ws_nrml(icL) = ws_nrml(icL) + ws_max*af
+      ws_nrml(icR) = ws_nrml(icR) + ws_max*af
+    enddo interior_edges
+
+
+    !--------------------------------------------------------------------------!
+    ! compute left and right fluxes for boundary edges/faces
+    !--------------------------------------------------------------------------!
+    nboundaries: do ib=1,nbndries
+      boundary_edges: do i=1,bndry(ib)%nedges
+
+        !-- get global edge number
+        ie = bndry(ib)%edge(i)
+
+        !-- edge properties
+         xf = edge(ie)%x
+         yf = edge(ie)%y
+         af = edge(ie)%area
+        nxf = edge(ie)%nx
+        nyf = edge(ie)%ny
+
+        !-- left cell index and coordinates
+        icL= bndry(ib)%cell(i)
+        xcL= cell(icL)%x
+        ycL= cell(icL)%y
+
+        !-- compute left state
+        pfL(1:nvar) = pvar(1:nvar,icL) + phi_lim(icL) * ((xf-xcL) * grad(1:nvar,icL,1) + (yf-ycL) * grad(1:nvar,icL,2))
+
+        !-- compute right state
+        call bc_flux (xf,yf,bndry(ib)%type, pfR)
 
         !-- compute inviscid flux
-        call flux_invscid_roe (pfL, pfR, nxf,nyf,  flux)
+        call compute_flux_invscid (pfL, pfR, nxf,nyf,  flux, ws_max)
 
         resid(1:nvar,icL) = resid(1:nvar,icL) + flux(1:nvar)*af
-      enddo
-    enddo interior_cells
+
+        ws_nrml(icL) = ws_nrml(icL) + ws_max*af
+      enddo boundary_edges
+    enddo nboundaries
 
 
-    boundary_cells: do ic=1,ncells_bndr
-      icL=cell_bndr(ic)
-      do ieL=1,cell(icL)%nvrt
-
-        je=cell(icL)%edge(ieL)
-
-        af =edge(je)%area
-        nxf=edge(je)%nx * cell(icL)%nrmlsign(ieL)
-        nyf=edge(je)%ny * cell(icL)%nrmlsign(ieL)
-
-        !-- find cell and edge number of right state
-        icR=cell(icL)%nghbre(ieL)
-        if (icR>0) then
-          idum(:)= 1
-          do k=1,cell(icR)%nvrt
-            idum(k)=abs( je - cell(icR)%edge(k) )
-          enddo
-          iloc = minloc(idum)
-          ieR=maxval(iloc)
-          if (cell(icR)%edge(ieR)/=je) write(*,*) 'no way'
-        else
-          icR=icL
-          ieR=ieL
-        endif
-
-        !-- construct left and right state on each edge
-        pfL(1:nvar) = recnst(1:nvar,icL)%f(ieL)
-        pfR(1:nvar) = recnst(1:nvar,icR)%f(ieR)
-
-        !-- compute inviscid flux
-        call flux_invscid_roe (pfL, pfR, nxf,nyf,  flux)
-
-        resid(1:nvar,icL) = resid(1:nvar,icL) + flux(1:nvar)*af
-      enddo
-    enddo boundary_cells
-
-
-    ! do icL=1,ncells
-    !   do ieL=1,cell(icL)%nvrt
-    !
-    !     je=cell(icL)%edge(ieL)
-    !
-    !     af =edge(je)%area
-    !     nxf=edge(je)%nx * cell(icL)%nrmlsign(ieL)
-    !     nyf=edge(je)%ny * cell(icL)%nrmlsign(ieL)
-    !
-    !     !-- find cell and edge number of right state
-    !     icR=cell(icL)%nghbre(ieL)
-    !     if (icR>0) then
-    !       idum(:)= 1
-    !       do k=1,cell(icR)%nvrt
-    !         idum(k)=abs( je - cell(icR)%edge(k) )
-    !       enddo
-    !       iloc = minloc(idum)
-    !       ieR=maxval(iloc)
-    !       if (cell(icR)%edge(ieR)/=je) write(*,*) 'no way'
-    !     else
-    !       icR=icL
-    !       ieR=ieL
-    !     endif
-    !
-    !     !-- construct left and right state on each edge
-    !     pfL(1:nvar) = recnst(1:nvar,icL)%f(ieL)
-    !     pfR(1:nvar) = recnst(1:nvar,icR)%f(ieR)
-    !
-    !     !-- compute inviscid flux
-    !     call flux_invscid_roe (pfL, pfR, nxf,nyf,  flux)
-    !
-    !     resid(1:nvar,icL) = resid(1:nvar,icL) + flux(1:nvar)*af
-    !   enddo
-    ! enddo
+    ! dQ/dt = -residual
+    resid = -resid
 
     return
   end subroutine compute_residual
+
+
+  subroutine bc_flux (x,y, bc_type, pfR)
+    implicit none
+
+    real,intent(in)       :: x,y
+    character,intent(in)  :: bc_type*80
+    real,intent(out)      :: pfR(nvar)
+    real                  :: rhs(nvar)
+
+    pfR = 0.d0
+
+    select case(trim(bc_type))
+
+    case('freestream')
+      !call bc_flux_freestream(wb)
+
+    case('slip_wall')
+      !call bc_flux_slipwall(wL,njk, wb)
+
+    case('solid_wall')
+      !call bc_flux_solidwall(wL, wb)
+
+    case('dirichlet')
+      if (lvortex) then
+        pfR(1:nvar) = vortex_inf(1:nvar)
+      else
+        call mms_compute_euler2d (x,y,pfR,rhs)
+      endif
+
+    case default
+      write(*,*) "Boundary condition=",trim(bc_type),"  not implemented."
+      stop
+
+    end select
+
+    return
+  end subroutine bc_flux
 
 
 end module residual
