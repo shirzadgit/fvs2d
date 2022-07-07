85b7211a0b6f8e967b3f15476dca7dee23e01cae
shirzadgit <shirzadh@email.arizona.edu>
2022-07-07 14:19:41 -0700

    updated

--- src/mainparam.f90
+++ src/mainparam.f90
@@ -2,7 +2,7 @@ module mainparam
   implicit none
 
   !--number of variables (2d --> nvar=4, rho,u,v,p;   3d --> nvar=4, rho,u,v,w,p)
-  integer,parameter       :: nvar=4
+  integer,parameter       :: nvar = 4, grid_dim = 2
 
   !-- #'s MPI processors
   integer,save            :: nproc,proc_id
@@ -12,14 +12,24 @@ module mainparam
   logical,save            :: lOMP
 
   !-- input file
-  character(len=127)      :: file_input='input.fvs2d'
+  character(len=127)      :: file_input='fvs2d.input'
   integer,parameter       :: iunit_input=900
 
   !-- grid file
+  character(len=5)        :: grid_ext='.grid'
+  character(len=3)        ::   bc_ext='.bc'
   integer,parameter       :: iunit_grid=901
+  integer,parameter       :: iunit_bc=902
+
+  !-- isentropic vortex
+  character(len=15)       :: file_vortex='fvs2d.vortex'
+  integer,parameter       :: iunit_vortex=903
 
   !-- log files
-  character(len=127)      :: file_output='log.fvs2d'
-  integer,parameter       :: iunit_output=902
+  character(len=127)      :: file_log_input='log.fvs2d'
+  integer,parameter       :: iunit_log_input=904
+
+  character(len=127)      :: file_log_grid='log.grid'
+  integer,parameter       :: iunit_log_grid=905
 
 end module mainparam
