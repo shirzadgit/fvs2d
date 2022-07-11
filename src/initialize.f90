23b14232e3749841c1ebdc1f329d20d044f27537
shirzadgit <shirzadh@email.arizona.edu>
2022-07-11 15:25:35 -0700

    minor updates

--- src/initialize.f90
+++ src/initialize.f90
@@ -1,7 +1,7 @@
 module initialize
 
   use input,        only  : ntstart, dt, lvortex
-  use data_grid,    only  : ncells, cell
+  use data_grid,    only  : ncells, cell, bndry, nbndries
   use data_solution
   use mms,          only  : mms_sol, compute_isentropic_vortex
 
@@ -17,11 +17,11 @@ contains
   subroutine initialize_solution
 
     implicit none
-    integer     :: ic
-    real        :: t_inital, pvar_vortex(nvar)
+    integer     :: ic, ib, i
+    real        :: t_initial, pvar_vortex(nvar)
 
 
-    t_inital = dble(ntstart-1)*dt
+    t_initial = dble(ntstart-1)*dt
 
 
     !-- set zero
@@ -37,10 +37,19 @@ contains
         pvar(ip,ic) =   p_inf
       enddo cell_loop1
 
+      ! do ib=1,nbndries
+      !   if (trim(bndry(ib)%type) == 'slip_wall') then
+      !     do i=1,bndry(ib)%ncells
+      !       ic=bndry(ib)%cell(i)
+      !       pvar(iu,ic) =   u_inf*0.7
+      !     enddo
+      !   endif
+      ! enddo
+
       !-- initialize with isentropic vortex
       if (lvortex) then
         cell_loop2 : do ic = 1, ncells
-          call compute_isentropic_vortex (cell(ic)%x, cell(ic)%y, pvar_vortex)
+          call compute_isentropic_vortex (t_initial, cell(ic)%x, cell(ic)%y, pvar_vortex)
           pvar(ir,ic) = pvar_vortex(ir)
           pvar(iu,ic) = pvar_vortex(iu)
           pvar(iv,ic) = pvar_vortex(iv)
--- src/io.f90
+++ src/io.f90
@@ -1,6 +1,7 @@
 module io
 
   use mainparam
+  use input,        only  : dtsave
   use data_grid
   use data_solution
   use tecplot
@@ -15,21 +16,20 @@ contains
   !============================================================================!
   !\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\!
   !============================================================================!
-  subroutine io_write_inst
+  subroutine io_write_inst (sol_time,ntimes_sub)
     implicit none
 
+    real,intent(in)             :: sol_time
+    integer,intent(in)          :: ntimes_sub
     integer                     :: ivar, np, i
     real(kind=4),allocatable    :: f4(:,:),xy4(:,:)
     real,allocatable            :: f8(:)
-    real                        :: sol_time
+
     character(len=*)            :: file_out*100, varinfo*100, dchar*5
 
 
-    counter = counter + 1
-    write(dchar,'(i5.5)') counter
-
-    !--
-    if (counter == 1) then
+    !-- write out grid file only once
+    if (counter == 0) then
       allocate(xy4(nnodes,2))
       do i=1,nnodes
         xy4(i,1) = node(i)%x
@@ -42,7 +42,16 @@ contains
     endif
 
 
+    !-- determine time-step number for output file
+    counter = counter + ntimes_sub
+    write(dchar,'(i5.5)') counter
+
+
+    !-- convert conservative variables to primitive variables
     call cvar2pvar
+
+
+    !-- interpolate cell center solution to to cell nodes
     allocate(f8(nnodes), f4(nnodes,nvar))
     do ivar=1,nvar
       call interpolate_cell2node(pvar(ivar,1:ncells), f8)
@@ -50,11 +59,10 @@ contains
     enddo
 
 
-    !--
+    !--write solution
     file_out = trim('solution_it'//dchar(1:5)//'.plt')
     varinfo  = 'rho u v p'
     np = 4
-    sol_time = dble(counter)
     call tecplot_write_solution (trim(file_out), trim(varinfo), np, f4, sol_time)
 
     deallocate(f4,f8)
