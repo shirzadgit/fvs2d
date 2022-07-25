9f5384fa01c7af1fa1c631fd4f4214e80958f68c
Shirzad Hosseinverdi <103480429+shirzadgit@users.noreply.github.com>
2022-07-25 13:40:04 -0700

    Delete make.sh

--- make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo ' '
-echo -n 'input compiler name (intel, pgi, cray): '; read F; FM="${F^^}"; 
-echo -n 'input mpi-wrapper (mpiifort, mpif90, ftn): '; read F; FC="${F,,}"; 
-echo ' '
-echo ' '
-sed -i "/FC=/c\FC=${FC}" "make.inc" 
-sed -i "/FM=/c\FM=${FM}" "make.inc"
-
